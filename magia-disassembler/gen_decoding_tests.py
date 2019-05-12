8221a4b425e8fedafa1b303e0c58118a0dd0f4a4
Daniel Collin <daniel@collin.com>
2019-05-12 09:26:00 +0200

    Added magia-disassembler based on the m68kdecode code

---
+++ magia-disassembler/gen_decoding_tests.py
@@ -0,0 +1,114 @@
+#! /usr/bin/env python
+
+import sys
+import os
+import re
+import subprocess
+
+R_NSYMCHAR = re.compile('[^a-zA-Z0-9_]+')
+R_UNDERSCORES = re.compile('[_]+')
+R_LEADING_UNDERSCORE = re.compile('^_')
+
+vasm = sys.argv[1]
+infilename = sys.argv[2]
+outfilename = sys.argv[3]
+
+tests = []
+
+with open(infilename, 'r') as f:
+    eof = False
+    while not eof:
+        asm_lines = []
+        result_lines = []
+
+        cpu = '68040'
+        while True:
+            l = f.readline()
+            if len(l) == 0:
+                eof = True
+                break
+            l = l.strip()
+            if len(l) == 0 or l.startswith('//'):
+                break
+            if l.startswith('> '):
+                asm_lines.append(l[1:])
+            elif l.startswith('# '):
+                result_lines.append(l[1:])
+            elif l.startswith('! '):
+                cpu = l[2:]
+            else:
+                stderr.write('bad input line: {}'.format(l))
+        
+        if len(asm_lines) > 0 and len(result_lines) > 0:
+            with open('test.asm', 'w') as of:
+                of.write('start:\n')
+                for line in asm_lines:
+                    of.write('\t')
+                    of.write(line)
+                    of.write('\n')
+            
+            if subprocess.call([vasm, '-m' + cpu, 'test.asm', '-quiet', '-Fbin', '-o', 'test.out']) != 0:
+                sys.stderr.write('vasm failed for:')
+                for l in asm_lines:
+                    sys.stderr.write(l)
+                sys.exit(1)
+            else:
+                with open('test.out', 'rb') as bf:
+                    code_bytes = bytearray(bf.read())
+                    tests.append((code_bytes, result_lines, asm_lines))
+
+
+    
+with open(outfilename, "w") as of:
+    of.write('// auto-generated from decoding_tests.txt by gen_decoding_tests.py\n')
+    of.write('mod tests {\n')
+    of.write('  use m68kdecode::*;\n')
+    of.write('  mod support;\n')
+    of.write('  use support::*;\n')
+    of.write('  use m68kdecode::DataRegister::*;\n')
+    of.write('  use m68kdecode::AddressRegister::*;\n')
+    of.write('  use m68kdecode::FloatingRegister::*;\n')
+    of.write('  use m68kdecode::MemoryIndirection::*;\n')
+    of.write('  use m68kdecode::FPConditionCode::*;\n')
+    of.write('  use m68kdecode::ConditionCode::*;\n')
+    of.write('  use m68kdecode::FPFormat::*;\n')
+    of.write('  use m68kdecode::Operand::*;\n')
+    of.write('  use m68kdecode::InstructionExtra::*;\n')
+    of.write('  use m68kdecode::BitfieldData::*;\n')
+    of.write('  use m68kdecode::Operation::*;\n')
+
+    testnum = 1
+    for t in tests:
+        code_bytes = t[0]
+        result_lines = t[1]
+        asm_lines = t[2]
+        for al in asm_lines:
+            of.write('// {}\n'.format(al))
+        name = re.sub(R_NSYMCHAR, '_', asm_lines[0].strip()).lower()
+        name = re.sub(R_UNDERSCORES, '_', name)
+        name = re.sub(R_LEADING_UNDERSCORE, '', name)
+        of.write('#[test]\n')
+        of.write('fn test_decode_{:04d}_{}() {{\n'.format(testnum, name))
+        if result_lines[0].find('Instruction {') == -1:
+            of.write('test_decoding_result_err(&[')
+        else:
+            of.write('test_decoding_result_ok(&[')
+        of.write(', '.join(['0x{0:02x}'.format(byt) for byt in code_bytes]))
+        of.write('], ')
+        for l in result_lines:
+            of.write(l)
+            of.write('\n')
+        of.write(', &[')
+        for al in asm_lines:
+            of.write('"{}",\n'.format(al))
+        of.write(']')
+        of.write(');}\n')
+
+        testnum = testnum + 1
+    of.write('}\n')
+
+if subprocess.call(['rustfmt', outfilename]) != 0:
+    sys.exit(1)
+
+os.remove('test.asm')
+os.remove('test.out')
---
+++ magia-disassembler/gendecoder.py
@@ -0,0 +1,218 @@
+#! /usr/bin/env python
+
+import re
+import sys
+import subprocess
+
+R_BLANK = re.compile('^\s*$')
+R_COMMENT = re.compile('^#.*')
+R_LINEFMT = re.compile('^([A-Z][A-Z0-9?]+)\s+((?:(?:(?:[01A-Za-z?]{4}_){3}[01A-Za-z?]{4})\s+)+)(.*?)$')
+R_PREDICATE = re.compile('\\?\\(.*?\\)')
+
+lineno = 0
+infile = sys.argv[1]
+outfile = sys.argv[2]
+
+class Capture(object):
+    def __init__(self, name, bit, length):
+        object.__init__(self)
+        self.name = name
+        self.bit = bit
+        self.length = length
+        self.wordindex = 0
+
+    def make_longer(self):
+        self.bit = self.bit - 1
+        self.length = self.length + 1
+
+    def __repr__(self):
+        return "{}({}/{}:{})".format(self.name, self.wordindex, self.bit, self.length)
+
+class Instruction(object):
+    def __init__(self, name, result):
+        object.__init__(self)
+        self.name = name
+        self.masks = []
+        self.instruction_patterns = []
+        self.captures = []
+        self.result = result
+
+    def add_mask(self, mask, instruction_pattern, captures):
+        self.masks.append(mask)
+        self.instruction_patterns.append(instruction_pattern)
+        for c in captures:
+            c.wordindex = len(self.masks) - 1
+            self.captures.append(c)
+
+    def match_expr(self, n):
+        return '(w{} & 0b{:016b}) == 0b{:016b}'.format(n, self.masks[n], self.instruction_patterns[n])
+
+    def __repr__(self):
+        return "{}:\n".format(self.name) +  \
+                "masks:\n    " + \
+                ' '.join(["{:016b}".format(m) for m in self.masks]) + \
+                "patterns:\n    " + \
+                ' '.join(["{:016b}".format(p) for p in self.instruction_patterns]) + \
+                "\ncaptures:\n    " + \
+                ' '.join([str(c) for c in self.captures]);
+
+def analyze_mask(mask):
+    assert len(mask) == 16
+
+    out_mask = 0
+    out_insn = 0
+
+    prev_capture = None
+    bit = 16
+    captures = []
+
+    for ch in mask:
+        bit = bit - 1
+        if ch == '0':
+            out_mask = out_mask | (1 << bit)
+            prev_capture = None
+        elif ch == '1':
+            out_insn = out_insn | (1 << bit)
+            out_mask = out_mask | (1 << bit)
+            prev_capture = None
+        elif ch == prev_capture:
+            captures[-1].make_longer()
+        else:
+            prev_capture = ch
+            captures.append(Capture(ch, bit, 1))
+            
+    return (out_mask, out_insn, captures)
+
+instructions = []
+
+with open(infile, "r") as inf:
+    for line in inf:
+
+        lineno = lineno + 1
+
+        if R_COMMENT.match(line):
+            continue
+        elif R_BLANK.match(line):
+            continue
+        m = R_LINEFMT.match(line)
+        if not m:
+            sys.stderr.write('{}({}): bad line\n'.format(infile, lineno))
+            sys.stderr.write('{}({}): line "{}"\n'.format(infile, lineno, line))
+            sys.exit(1)
+
+        name, bits, result = m.groups()
+
+        i = Instruction(name, result)
+
+        bits = bits.strip()
+        for mask in bits.replace('_', '').split():
+            am, ai, ac = analyze_mask(mask)
+            i.add_mask(am, ai, ac)
+
+        instructions.append(i)
+        #print(i)
+
+def gen_decoders(of, insns):
+    group = insns[0].instruction_patterns[0] >> 12
+
+    of.write('#[allow(non_snake_case)]\n')
+    of.write('#[allow(unused_mut)]\n')
+    of.write('fn decode_group_{0:04b}(w0: u16, cs: &mut CodeStream) -> Result<DecodedInstruction, DecodingError> {{\n'.format(group))
+
+    for i in insns:
+        of.write('if (w0 & 0b{:016b}) == 0b{:016b} '.format(i.masks[0], i.instruction_patterns[0]))
+        if len(i.masks) > 1:
+            of.write('&& cs.has_words({})'.format(len(i.masks) - 1))
+        of.write('{\n')
+        #of.write('println!("w0 match {}");\n'.format(i.name))
+        for n in range(1, len(i.masks)):
+            of.write('let w{} = cs.peek_word({});\n'.format(n, n-1))
+
+        if len(i.masks) > 1:
+            of.write('if {} {{\n'.format(' && '.join([i.match_expr(n) for n in range(1, len(i.masks))])))
+
+        for c in i.captures:
+            if c.name != '?':
+                of.write('let {} = get_bits(w{}, {}, {});\n'.format(c.name, c.wordindex, c.bit, c.length))
+                #of.write('println!("{} = {{}}", {});\n'.format(c.name, c.name))
+
+        predicate_nesting = 0
+
+        for predicate in re.findall(R_PREDICATE, i.result):
+            predicate_nesting = predicate_nesting + 1
+            of.write('if {} {{\n'.format(predicate[2:-1]))
+
+        if len(i.masks) > 1:
+            of.write('cs.skip_words({});\n'.format(len(i.masks)-1))
+
+        expr = re.sub(R_PREDICATE, "", i.result)
+
+        have_extra = False
+
+        for sub_expr in expr.split(';'):
+            sub_expr = sub_expr.strip();
+            if len(sub_expr) == 0:
+                continue
+            if not sub_expr.startswith('let'):
+                of.write('let ')
+            if sub_expr.find('extra') != -1:
+                have_extra = True
+            of.write(sub_expr)
+            of.write(';\n')
+
+        if not have_extra:
+            of.write('let extra = NoExtra;\n')
+
+        if expr.find('return') == -1:
+            of.write('return cs.check_overflow(Instruction {{ size: sz, operation: {}, operands: [ src, dst ], extra: extra }});\n'.format(i.name))
+
+        for x in range(0, predicate_nesting):
+            of.write('}\n')
+
+        if len(i.masks) > 1:
+            of.write('}\n')
+
+        of.write('}\n')
+
+    of.write('  return Err(DecodingError::NotImplemented);\n')
+    of.write('}\n')
+    
+with open(outfile, "w") as of:
+    of.write('use crate::*;\n')
+    of.write('use crate::Operand::*;\n')
+    of.write('use crate::InstructionExtra::*;\n')
+    of.write('use crate::Operation::*;\n')
+    of.write('use codestream::*;\n')
+
+    seen_insn_names = {}
+    of.write('/// Instruction names.\n');
+    of.write('#[derive(Debug, PartialEq, Clone)]\npub enum Operation {\n');
+    for i in instructions:
+        if not seen_insn_names.has_key(i.name):
+            seen_insn_names[i.name] = True
+            of.write('  {},'.format(i.name))
+    of.write('}\n');
+
+    has_group = {}
+
+    for group in range(0, 16):
+        insns = [i for i in instructions if (i.instruction_patterns[0] >> 12) == group]
+        if len(insns) == 0:
+            continue
+        has_group[group] = True
+        gen_decoders(of, insns)
+
+    of.write('pub fn decode_instruction_generated(code: &[u8]) -> Result<DecodedInstruction, DecodingError> {\n')
+    of.write('  let mut cs = CodeStream::new(code);\n')
+    of.write('  let w0 = cs.pull16();\n')
+    of.write('  match w0 >> 12 {\n')
+    for group in range(0, 16):
+        if not has_group.has_key(group):
+            continue
+        of.write('    0b{0:04b} => decode_group_{0:04b}(w0, &mut cs),\n'.format(group))
+    of.write('    _ => Err(DecodingError::NotImplemented)\n')
+    of.write('  }\n')
+
+    of.write('}\n')
+
+subprocess.call(['rustfmt', outfile])
