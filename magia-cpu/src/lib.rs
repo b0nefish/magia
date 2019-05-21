18c4dc1472c323ad09c967eab508db1efc44eeaa
Daniel Collin <daniel@collin.com>
2019-05-21 08:07:50 +0200

    Starting out

--- magia-cpu/src/lib.rs
+++ magia-cpu/src/lib.rs
@@ -1,7 +1,19 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+/// Data for the m68k CPU
+struct Cpu {
+    /// dn registers for the CPU
+    d_registers: [u32; 8],
+    /// an registers for the CPU
+    a_registers: [u32; 8],
+    /// First word of the instruction used for looking up handler
+    ir: u32,
+    /// Program counter
+    pc: u32,
 }
+
+impl Cpu {
+
+
+
+}
+
+
