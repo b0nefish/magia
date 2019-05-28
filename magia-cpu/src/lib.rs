021733b588b98a811255763f0072ff632d9d50d0
Daniel Collin <daniel@collin.com>
2019-05-28 12:52:50 +0200

    Current WIP

--- magia-cpu/src/lib.rs
+++ magia-cpu/src/lib.rs
@@ -1,3 +1,16 @@
+
+/// This is the state the CPU is currently in regarding execute and instruction decode
+enum DecodeExecState {
+    Decode,
+    Exec,
+}
+
+/// State for fetching of data.
+enum FetchState {
+    WaitFetch,
+    ReadWord,
+}
+
 /// Data for the m68k CPU
 struct Cpu {
     /// dn registers for the CPU
@@ -5,15 +18,27 @@ struct Cpu {
     /// an registers for the CPU
     a_registers: [u32; 8],
     /// First word of the instruction used for looking up handler
-    ir: u32,
+    irc: u16,
+    /// First word of the instruction used for looking up handler
+    ir: u16,
+    /// First word of the instruction used for looking up handler
+    ird: u16,
     /// Program counter
     pc: u32,
+    exec_state: DecodeExecState,
+    /// Number of cycles left for instruction decode
+    decode_cycles: u8,
+    /// How many cycles that are left in the exec state
+    exec_cycles_left: u8,
 }
 
 impl Cpu {
+    /// Step the CPU one cycle forward
+    fn step(cpu: &mut Cpu, memory_bus: &mut MemoryBus) {
 
 
-
+    }
 }
+
 
 
